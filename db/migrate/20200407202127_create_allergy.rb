d88cc4a6d11ccfc3e9a2ec2af7a074bc254f911c
devmullets <devmullets@gmail.com>
2020-04-07 17:53:33 -0400

    update migration files

--- db/migrate/20200407202127_create_allergy.rb
+++
@@ -1,6 +0,0 @@
-class CreateAllergy < ActiveRecord::Migration[6.0]
-  def change
-    create_table :allergies do |t|
-    end
-  end
-end
---
+++ db/migrate/20200407214119_create_allergen.rb
@@ -0,0 +1,6 @@
+class CreateAllergen < ActiveRecord::Migration[6.0]
+  def change
+    create_table :allergens do |t|
+    end
+  end
+end
